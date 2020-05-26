075180cbe63770b3aeae314781f7640af826d5f2
Sarah Estrellado <Sarah.estrellado@icloud.com>
2020-05-26 13:32:24 -0500

    added slider functionality

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        heightLabel.text = (NSString(format:"%.2f", sender.value)) as String
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        let weightValue = String(Int(ceilf(sender.value)))
+        weightLabel.text = weightValue
+       // print(sender.value)
+    }
 }
 
