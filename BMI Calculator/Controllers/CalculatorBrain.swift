37a93d0ae437c76b0b16c35b39b136dda57c012b
Sarah Estrellado <Sarah.estrellado@icloud.com>
2020-05-27 16:21:22 -0500

    refactored CalculatorBrain

--- BMI Calculator/Controllers/CalculatorBrain.swift
+++ BMI Calculator/Controllers/CalculatorBrain.swift
@@ -10,16 +10,15 @@ import Foundation
 
 struct CalculatorBrain {
 
-    var bmiValue = "0.0"
+    var bmiValue: Float = 0.0
     
-   mutating func calculateBMI(height: Float, weight: Float)  {
-    
-    let bmiResult = weight / pow(height, 2)
-    bmiValue = String(format: "%.1f", bmiResult)
+    mutating func calculateBMI(height: Float, weight: Float)  {
+    bmiValue = weight / pow(height, 2)
     }
     
     func getBMIValue() -> String {
-        return bmiValue
+       let bmiValueToReturn = String(format: "%.1f", bmiValue)
+        return bmiValueToReturn
     }
     
 }
